b720c8268a652eb69d8a53bc91bc6cc211c85d65
Eric Glasser <eglasser@trace3.com>
2018-10-19 15:53:13 -0600

    added join api

--- index.js
+++ index.js
@@ -5,12 +5,46 @@
 const Server = require('socket.io');
 const io = new Server();
 
-require('dotenv').config({ silent: true });
+var teams = {
+    red: 0,
+    blue: 0
+};
 
 const express = require('express');
 
 let app = express();
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/join', function (req, res) {
+    if (teams.red > teams.blue) {
+        addTeamBlue(res);
+    } else if (teams.red < teams.blue) {
+        addTeamRed(res);
+    } else {
+        if(Math.random() > .5) {
+            addTeamRed(res);
+        } else {
+            addTeamBlue(res);
+        }
+    }
+})
+
+function addTeamRed(res) {
+    teams.red++;
+    return res.json({
+        team: "red",
+        numbers: teams
+    });
+}
+
+function addTeamBlue(res) {
+    teams.blue++;
+    return res.json({
+        team: "blue",
+        numbers: teams
+    });
+}
+
 
 module.exports = app;
